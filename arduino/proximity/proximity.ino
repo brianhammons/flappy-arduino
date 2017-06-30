0d015d1285f428d4115cf23064185fadee8e8735
Brian <hammbri@amazon.com>
2017-06-29 23:32:16 -0700

    Add working integration with arduino

---
+++ arduino/proximity/proximity.ino
@@ -0,0 +1,38 @@
+#define trigPin 13
+#define echoPin 12
+#define led 11
+#define led2 9
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 10) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Green condition is met, the Red LED should turn off
+  digitalWrite(led2,LOW);
+}
+  else {
+    digitalWrite(led,LOW);
+    digitalWrite(led2,HIGH);
+  }
+  if (distance >= 200 || distance <= 0){
+    Serial.println(199); // 199 represents "out of range"
+  }
+  else {
+    Serial.println(distance);
+  }
+  delay(200);
+}
